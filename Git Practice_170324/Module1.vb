ecd8fba9dd4bfdfb19226bb58de4d6586a756af2
SimPC <User@USER-PC>
2017-03-24 17:05:09 +0900

    second

---
+++ Git Practice_170324/Module1.vb
@@ -0,0 +1,18 @@
+﻿Module Module1
+    '
+    Public Sub Practice()
+        Dim a As Single
+        Dim b As Single
+        '
+        Dim c As Single
+        '
+        a = 1
+        b = 2
+        c = a * b
+        '
+        c = a + b
+        '
+        c = a - b
+        '
+    End Sub
+End Module
